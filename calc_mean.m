59bd967785f24b125aa4c5d749fe3a1e4e802081
Yiying Li <yyli@umich.edu>
2013-04-14 23:28:18 -0400

    presentation commit

--- calc_mean.m
+++ calc_mean.m
@@ -1,15 +1,17 @@
-function [] = gen_depth_all(theta)
-    folder = 'Test134Img';
+function [diff] = calc_mean()
+    folder = 'Testresults/depth/';
 	listing = dir(folder);
 	numfiles = size(listing, 1) - 2;
+    x_max = 213;
+    y_max = 284;
+    diff = zeros(1, numfiles);
     for i = 1:numfiles,
-        disp(listing(i+2).name);
-        img = imread(listing(i+2).name);
-        load(strcat('depth_sph_corr-', listing(i+2).name(1+4:end-4), '.mat'));
-        depth = log(resize_depth(Position3DGrid(:, :, 4), 213, 284));
-        fea_vector = generate_all_image_features(img);
-        gdepth = inference_depth(fea_vector, theta);
-        save(strcat('Testresults/depth/', listing(i+2).name(1+4:end-4), '.mat'), 'gdepth', '-v7.3');
+        % disp(listing(i+2).name);
+        load(strcat('depth_sph_corr-', listing(i+2).name));
+        depth = log(resize_depth(Position3DGrid(:, :, 4), x_max, y_max));
+        load(listing(i+2).name);
+        absd = mean(mean(abs(depth-gdepth)));
+        diff(i) = absd;
 %         lim = [min(min(depth)) max(max(depth))];
 %         maxd = max(max(depth));
 %         maxgd = max(max(gdepth));
@@ -26,4 +28,7 @@ function [] = gen_depth_all(theta)
 %         saveas(h, strcat('testresults/result-', listing(i+2).name(1+4:end-4), '.jpg'), 'jpg');
 %         close(h);
     end
+    diff;
+    mean(diff)
+    exp(mean(diff))
 end
